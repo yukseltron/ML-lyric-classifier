35d8463d3fdfcab5d13bae496f8a39c196b59ab6
yukseltron <h.yuksel04@gmail.com>
2022-08-02 17:25:41 -0400

    deploy site test

---
+++ request.py
@@ -0,0 +1,10 @@
+import requests
+import json
+
+url = 'http://0.0.0.0:5000/api/'
+
+data = [["baby you can drive my car"]]
+j_data = json.dumps(data)
+headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
+r = requests.post(url, data=j_data, headers=headers)
+print(r, r.text)
