8c7ecd79fc0aa71998cbe10804a420e860f3ee3f
yukseltron <h.yuksel04@gmail.com>
2022-08-04 11:16:48 -0400

    modify json input

--- request.py
+++ request.py
@@ -1,10 +1,9 @@
 import requests
 import json
 
-url = 'https://lyrics-classifier.vercel.app/api/'
+url = 'http://0.0.0.0:5000/api/'
 
 data = [["baby you can drive my car"]]
-j_data = json.dumps(data)
 headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
-r = requests.post(url, data=j_data, headers=headers)
-print(r, r.text)
+r = requests.post(url, json={'lyric':data,}, headers=headers)
+print(r, r.json())
---
+++ server.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, redirect, url_for, flash, jsonify
+import numpy as np
+import pickle as p
+import json
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "<h1>Lyrics Classifier</h1>"
+
+@app.route('/api/', methods=['POST'])
+def makecalc():
+    data = request.get_json(force=True)
+    prediction = np.array2string(model.predict(data['lyric']))
+    print(prediction)
+    return jsonify(prediction)
+
+
+if __name__ == '__main__':
+    modelfile = 'model.pickle'
+    model = p.load(open(modelfile, 'rb'))
+    app.run(debug=True, host='0.0.0.0')
