841b8bd81683947ef9d805b01652123a68ba6a89
yukseltron <h.yuksel04@gmail.com>
2022-08-05 18:48:00 -0400

    rendered flask app

--- venv/index.py
+++ venv/index.py
@@ -1,23 +1,42 @@
-from flask import Flask, request, redirect, url_for, flash, jsonify
+from flask import Flask, request, redirect, url_for, flash, jsonify, render_template
 import numpy as np
 import pickle as p
 import json
-
+from werkzeug.exceptions import HTTPException
 
 app = Flask(__name__)
 
+messages = [{'title': 'Message One',
+             'content': 'Message One Content'},
+            {'title': 'Message Two',
+             'content': 'Message Two Content'}
+            ]
+
 @app.route("/")
-def home():
-    return "<h1>Lyrics Classifier</h1>"
+def index():
+    return render_template('index.html', messages=messages)
 
 @app.route('/api/', methods=['POST'])
-def makecalc():
+def get_calc():
     data = request.get_json(force=True)
-    prediction = np.array2string(model.predict(data['lyric']))
-    print(prediction)
+    prediction = np.array2string(model.predict([data['lyric']]))
     return jsonify(prediction)
 
+@app.errorhandler(HTTPException)
+def handle_exception(e):
+    """Return JSON instead of HTML for HTTP errors."""
+    # start with the correct headers and status code from the error
+    response = e.get_response()
+    # replace the body with JSON
+    response.data = json.dumps({
+        "code": e.code,
+        "name": e.name,
+        "description": e.description,
+    })
+    response.content_type = "application/json"
+    return response
 
 if __name__ == '__main__':
     modelfile = 'model.pickle'
     model = p.load(open(modelfile, 'rb'))
+    app.run(debug=True, host='0.0.0.0')
--- venv/request.py
+++ venv/request.py
@@ -1,9 +1,9 @@
 import requests
 import json
 
-url = 'http://0.0.0.0:5000/api/'
+url = 'https://lyrics-classifier-yukseltron.vercel.app/api/'
 
-data = [["baby you can drive my car"]]
+data = {'lyric': 'baby you can drive my car',}
 headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
-r = requests.post(url, json={'lyric':data,}, headers=headers)
+r = requests.post(url, data=json.dumps(data), headers=headers)
 print(r, r.json())
